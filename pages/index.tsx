e7e083cbba5ec1680c27fb03a70c309cf97185d2
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2022-08-21 18:38:11 -0300

    bootstrap & bootswatch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootswatch/dist/superhero/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+
 const Home: NextPage = () => {
   return (
     <div className="container mx-auto">
@@ -9,7 +14,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Czar Storage App - Czar.dev" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Button variant="primary">Primary</Button>
       <h1 className="text-6xl font-normal leading-normal mt-0 mb-2 text-pink-800">
         Tailwind Starter Kit
       </h1>
